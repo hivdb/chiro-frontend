8c6c7fffb33cb8e581bee2d47137f8a3e5e662b5
Philip Tzou <philiptz@stanford.edu>
2020-03-21 19:11:07 -0700

    Prepare for release

--- src/apollo.js
+++ src/apollo.js
@@ -2,14 +2,18 @@ import {ApolloClient} from 'apollo-client';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 import {HttpLink} from 'apollo-link-http';
 
+let uri = 'http://localhost:5000/graphql';
+
+if (window.__NODE_ENV === 'production') {
+  uri = '/graphql';
+}
+
 const cache = new InMemoryCache();
-const link = new HttpLink({
-    uri: 'http://localhost:5000/graphql',
-});
+const link = new HttpLink({uri});
 
 
 export default new ApolloClient({
   link, cache,
   name: 'chiro-web-client',
   version: '0.1'
-})
+});
