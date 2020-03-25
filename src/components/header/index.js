dc12ebdf27369943765d6c2480e53a00a7a12b49
Kaiming <mads396@gmail.com>
2020-03-24 18:24:32 -0700

    add site header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import style from './style.module.scss';
+
+import './header.sass';
 
 
 export default class MockHeader extends React.Component {
 
   render() {
-    return <div className={style.header} />;
+    return (
+      <>
+        <header className="header-bar">
+          <nav className="header-container">
+            <ul className="nav">
+              <li className="nav-item">
+                <a href="/">Home</a>
+              </li>
+              <li className="nav-item">
+                <a href="/">Literatures</a>
+              </li>
+              <li className="nav-item">
+                <a href="/search">Experiments</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <header className="header-global header-logo">
+          <nav className="header-container">
+            <a className="brand-logo" href="/">Stanford Medicine</a>
+            <a className="brand-sitename" href="/">CoV-Rx-DB</a>
+          </nav>
+
+        </header>
+      </>
+    );
   }
 
 }
