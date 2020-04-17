f86249a6a391e384ae554658010bfeffa819bdee
Kaiming <mads396@gmail.com>
2020-04-17 11:54:58 -0700

    update statistic page

--- src/views/statistics/index.js
+++ src/views/statistics/index.js
@@ -3,22 +3,26 @@ import CompoundChart from './compound.svg';
 import PolymeraseChart from './polymerase.svg';
 import ClinialChart from './clinical-trials.svg';
 import TargetChart from './target.svg';
+import VirusChart from './virus.svg';
 
 
 export default function Statistics() {
   // return <TargetChart />;
   return (<>
     <div>
-      <img src={PolymeraseChart} alt={"Polymerase"} width={1000}/>
+      <img src={VirusChart} alt={"virus"} width={1000} />
     </div>
     <div>
       <img src={TargetChart} alt={"Target"} width={1000} />
     </div>
     <div>
-      <img src={CompoundChart} alt={"Compound"} width={1000}/>
+      <img src={PolymeraseChart} alt={"Polymerase"} width={1000}/>
     </div>
     <div>
       <img src={ClinialChart} alt={"Clinical trials"} width={1000}/>
     </div>
+   <div>
+      <img src={CompoundChart} alt={"Compound"} width={1000}/>
+   </div>
   </>)
 }
